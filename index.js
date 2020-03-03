ef0f3480b90109477f40752f3ec7f0b56c442be5
Rohit <puthenrohit@gmail.com>
2020-03-03 09:18:30 +0530

    Provision to utter randomly from multiple utterances available without repeating the same thing to the same user - optional

--- index.js
+++ index.js
@@ -1,1 +1,88 @@
-const response={events:[],responses:[{text:""}]},handleFormAction=(e,t,n,s,l)=>new Promise((o,r)=>{let a=JSON.parse(JSON.stringify(response));const u=()=>{try{let t=e.length-1;for(let r in e){let u=e[r];if(null===n[u]){let e="utter_ask_"+u;a.responses[0].text=s[e][0].text,a.events.push({event:"slot",name:"components",value:"text_msg"}),a.events.push({event:"slot",name:"requested_slot",value:u}),o(a);break}t==r&&(a.events.push({event:"followup",name:l}),o(a))}}catch(e){r(e)}};if(t.length){const s=t.length-1;for(let l in t){let o=t[l];e.includes(o.entity)&&(a.events.push({event:"slot",name:o.entity,value:o.value}),n[o.entity]=o.value),s==l&&u()}}else u()});module.exports.handleFormAction=handleFormAction;
+
+const response = {
+    events: [],
+    responses: [{
+        text: ""
+    }]
+};
+
+let lastResp = {}; 
+
+Array.prototype.randomDiffResp = function(last) {
+   if (this.length == 0) {
+      return;
+   } else if (this.length == 1) {
+      return this[0];
+   } else {
+      let num = 0;
+      do {
+         num = Math.floor(Math.random() * this.length);
+      } while (this[num] == last);
+      return this[num];
+   }
+}
+
+
+const handleFormAction = (requiredSlots, entities, slots, templates, submitAction, senderId='')=>{
+return new Promise((resolve, reject)=>{
+const nextUtter = 'utter_ask_', respData = JSON.parse(JSON.stringify(response));
+        const nextSlot = ()=>{
+            try{
+                const maxIter = requiredSlots.length - 1;
+                for (let i in requiredSlots){
+                    const currSlot = requiredSlots[i];
+                    if (slots[currSlot] === null){
+                        const nextSlotName = nextUtter+currSlot;
+                        if(senderId === ''){
+                            respData.responses[0].text = templates[nextSlotName].randomDiffResp().text;   
+                        }
+                        else{
+                            if(!(senderId in lastResp)){
+                               lastResp[senderId] = {};
+                               lastResp[senderId][nextSlotName] = '';
+                            }
+                            const last = lastResp[senderId][nextSlotName];
+                            const nextReply = templates[nextSlotName].randomDiffResp(last).text;
+                            respData.responses[0].text = nextReply;
+                            lastResp[senderId][nextSlotName] = nextReply;
+                        }
+                        respData.events.push({event: "slot",name: "components",value:'text_msg'});
+                        respData.events.push({event:"slot", name:"requested_slot", value:currSlot});
+                        resolve(respData);
+                        break;
+                    }
+                    else if(maxIter == i){
+                        //got all slots - submit function
+                        respData.events.push({event: "followup", name: submitAction});
+                        resolve(respData);
+                    }
+                }
+            }
+            catch(e){
+                reject(e);
+            }
+        }
+
+    if(entities.length){
+        const num_entities = entities.length - 1;
+        for(let x in entities){
+            const ner = entities[x];
+            if(requiredSlots.includes(ner.entity)){
+                respData.events.push({event:"slot", name:ner.entity, value:ner.value});
+                slots[ner.entity] = ner.value;
+            }
+            if(num_entities == x){
+                nextSlot();
+            }
+        }
+    }
+    else{
+        nextSlot();
+    }
+});
+}
+
+
+module.exports.handleFormAction = handleFormAction;
+
+module.exports.response = response;
